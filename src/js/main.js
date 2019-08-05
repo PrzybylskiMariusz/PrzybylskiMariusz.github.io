95185e823aec0a41bea13d690fc85386473e76ed
Mariusz Przybylski <przybylski.mariusz1987@gmail.com>
2019-08-05 17:44:45 +0200

    feat: refactor after code review

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
-console.log(`Welcome to my world! Nice to see you :)`);
+console.log(`Welcome to my world! Nice to see you 😃`);
 
 
 const list = document.querySelector('.repos--js');
 
-fetch("https://api.github.com/users/PrzybylskiMariusz/repos")
+fetch("https://api.github.com/users/PrzybylskiMariusz/repos?sort=updated&direction=desc")
   .then(resp => resp.json())
   .then(resp => {
   const repos = resp;
@@ -13,13 +13,17 @@ fetch("https://api.github.com/users/PrzybylskiMariusz/repos")
     const {html_url, name, description, homepage} = repo;
     list.innerHTML += `<li class="repos__list">
     <div class="repos__info">
-      <img class="repos__icon" src="assets/img/github-icon-big.png" alt="github icon.">
-      <h4 class="repos__title">${name}</h4>
-      <p class="repos__description">${description}</p>
+      <img class="repos__icon" src="assets/img/github-icon-big.svg" alt="">
+      <h3 class="repos__title">${name}</h3>
+      ${
+        description ? `<p class="repos__description">${description}</p>` : 'There is no description for this project 😞'
+      }
     </div>
     <div class="repos__links">
-      <a class="repos__link repos__link--demo" href="${homepage}" target="_blank" rel="noopener noreferrer"><img class="repos__image" src="assets/img/demo-icon.png" alt="demo icon.">Demo</a>
-      <a class="repos__link repos__link--github" href="${html_url}" target="_blank" rel="noopener noreferrer"><img class="repos__image" src="assets/img/code-icon.png" alt="demo icon.">Github</a>
+      ${
+        homepage ? `<a class="repos__link repos__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="Demo: ${name}."><img class="repos__image" src="assets/img/demo-icon.svg" alt="demo icon.">Demo</a>` : ''
+      }
+      <a class="repos__link repos__link--github" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="Source code: ${name}."><img class="repos__image" src="assets/img/code-icon.svg" alt="code icon.">Github</a>
     </div>
   </li>`;
   }
